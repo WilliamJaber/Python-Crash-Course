f99b9f4a25f6dfa49bd97a4aed9dd41bffbb0073
William Jaber <90568326+WilliamJaber@users.noreply.github.com>
2021-09-24 16:53:35 -0500

    Delete Chapter 1 directory

--- Chapter 1/simple _messages.py
+++
@@ -1,5 +0,0 @@
-simple_message = 'assigning a messaage to a variable and printing it'
-print(simple_message)
-
-simple_messages = 'assigning a NEW value to the old variable and printing it'
-print(simple_messages)
--- Chapter 1/simple_message.py
+++
@@ -1,2 +0,0 @@
-simple_message = 'assigning a messaage to a variable and printing it'
-print(simple_message)
