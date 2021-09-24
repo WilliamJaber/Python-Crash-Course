f0bc63d4494a683514f1b4cde03bf90c85103c33
William <williamjaber94@gmail.com>
2021-09-23 19:38:53 -0500

    2-2. Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.

--- simple _messages.py
+++ simple _messages.py
@@ -1,1 +1,6 @@
+simple_message = 'assigning a messaage to a variable and printing it'
+print(simple_message)
 
+
+simple_messages = 'assigning a NEW value to the old variable and printing it'
+print(simple_messages)
