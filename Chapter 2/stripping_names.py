20af3d5ba940cc7fecd6ca27d190e1ea145b59d3
William <williamjaber94@gmail.com>
2021-09-30 16:29:37 -0500

    2-10. Adding Comments

--- Chapter 2/stripping_names.py
+++ Chapter 2/stripping_names.py
@@ -1,8 +1,12 @@
 my_name = '  William Jaber  '
 print(my_name)
 
+# stripping 'left' spaces
 print(my_name.lstrip())
+
+# stripping 'right' spaces
 print(my_name.rstrip())
+
 print(my_name.strip())
 
 
