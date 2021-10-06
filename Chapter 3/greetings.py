b38892c975b87c5bf84a36d223e521365f0aef09
William <williamjaber94@gmail.com>
2021-10-05 21:46:18 -0500

    3-2. Greetings

---
+++ Chapter 3/greetings.py
@@ -0,0 +1,10 @@
+names = ['Dani', 'Ale', 'E. Jose']
+
+message = f'Hey {names[0]}, Thanks for your friendship'
+print(message)
+
+message = f'Hey {names[1]}, Thanks for your friendship'
+print(message)
+
+message = f'Hey {names[2]}, Thanks for your friendship'
+print(message)
