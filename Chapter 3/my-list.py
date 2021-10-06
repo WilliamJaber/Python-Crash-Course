2acfc3031006b6392a5079196c9167cccca6f6ba
William <williamjaber94@gmail.com>
2021-10-05 21:46:35 -0500

    3-3. Your Own List

---
+++ Chapter 3/my-list.py
@@ -0,0 +1,10 @@
+my_list = ['motorcycle', 'cars', 'airplane']
+
+message = f'Where I live, having a {my_list[0]} is the fastest mode of transport.'
+print(message)
+
+message = f'German {my_list[1]} are my favorites.'
+print(message)
+
+message = f'{my_list[0]} is the most safe mode of transport.'
+print(message)
