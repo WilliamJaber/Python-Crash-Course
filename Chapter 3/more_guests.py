9d4063ace1ad5ab433d794e30c4c40c4dde45eef
William <williamjaber94@gmail.com>
2021-10-09 21:32:16 -0500

    3-6. More Guests

---
+++ Chapter 3/more_guests.py
@@ -0,0 +1,15 @@
+guest_list = ['Alex', 'Dan', 'Dave']
+
+guest_list.insert(0, 'Greg')
+guest_list.insert(3, 'Sam')
+guest_list.append('Eddy')
+
+print(f'Hey {guest_list[0]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[1]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[2]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[3]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[4]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[5]}! you are invited for my celebration dinner next week.')
+
+# for guest in guest_list:
+#     print(f'Hey {guest}! you are invited for my celebration dinner next week')
