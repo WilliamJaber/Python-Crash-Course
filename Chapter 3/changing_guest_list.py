9a78a50de92b3b18bcbf9661250ee8560dc4a54d
William <williamjaber94@gmail.com>
2021-10-09 21:32:04 -0500

    3-5. Changing Guest List

---
+++ Chapter 3/changing_guest_list.py
@@ -0,0 +1,9 @@
+guest_list = ['Alex', 'Dan', 'Edd']
+
+guest_list[-1] = 'Dave'
+
+print(f'Hey {guest_list[0]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[1]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[2]}! you are invited for my celebration dinner next week.')
+
+print("Edd can´t make it.")
