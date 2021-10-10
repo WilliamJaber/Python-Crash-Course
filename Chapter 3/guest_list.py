1b53fc5622d17cc9af1079f33d8b4837df2a3e4c
William <williamjaber94@gmail.com>
2021-10-09 21:31:43 -0500

    3-4. Guest List

---
+++ Chapter 3/guest_list.py
@@ -0,0 +1,5 @@
+guest_list = ['Alex', 'Dan', 'Edd']
+
+print(f'Hey {guest_list[0]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[1]}! you are invited for my celebration dinner next week.')
+print(f'Hey {guest_list[2]}! you are invited for my celebration dinner next week.')
