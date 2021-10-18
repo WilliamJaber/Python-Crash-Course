8dea8f78fee65837ea43a9057ef16295a1f4e401
William <williamjaber94@gmail.com>
2021-10-18 11:30:36 -0500

    4-10. Slices

---
+++ Chapter 4/slices.py
@@ -0,0 +1,7 @@
+animals = ['monkey', 'cat', 'squirrel', 'dog', 'turtle', 'snake', 'hamster']
+
+print(f'\nThe first three items  in the list are: {animals[:3]}\n')
+
+print(f'\nThe three items from the middle in the list are: {animals[2:5]}\n')
+
+print(f'\nThe last three items in the list are: {animals[4:]}\n')
