d6e7e9593a9803a2ee8584cc062b4f555cf11b8a
William <williamjaber94@gmail.com>
2021-10-18 11:31:03 -0500

    4-12. More Loops

---
+++ Chapter 4/more_loops.py
@@ -0,0 +1,13 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+first_list = []
+for food in my_foods:
+    first_list.append(food)
+print(f'my food:\n {first_list}')
+
+
+second_list = []
+for food in friend_foods:
+    second_list.append(food)
+print(f'\nfriend food:\n {second_list}')
