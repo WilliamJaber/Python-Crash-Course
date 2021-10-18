1b1c4d6594b07b0aeb89602d996ef8d8ff6e96e4
William <williamjaber94@gmail.com>
2021-10-17 21:27:43 -0500

    4-1. Pizzas

---
+++ Chapter 4/pizzas.py
@@ -0,0 +1,7 @@
+pizzas = ['pepperoni', 'margarita', 'prosciutto']
+
+for pizza in pizzas:
+    print(f'The {pizza} pizza is the best\n')
+
+print('\nThere are different types of pizza,\nbut the woodfired are special')
+print('I really love pizza!')
