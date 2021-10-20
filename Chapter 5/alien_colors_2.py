4591f955158a382a89bd4ed649e309ce1eebe8c6
William <williamjaber94@gmail.com>
2021-10-20 13:08:13 -0500

    5-4. Alien Colors #2

---
+++ Chapter 5/alien_colors_2.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien.')
+else:
+    print('You earned 10 points for shooting the alien.')
+
+# Write one version of this program that runs the if block and another that runs the else block.
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien.')
+else:
+    print('You earned 10 points for shooting the alien.')
