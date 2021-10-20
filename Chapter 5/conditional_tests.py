b8d8273dde1a683be785f5f41968b8b4aa50948c
William <williamjaber94@gmail.com>
2021-10-19 21:09:52 -0500

    5-1. Conditional Tests

---
+++ Chapter 5/conditional_tests.py
@@ -0,0 +1,34 @@
+favorite_food = 'pizza'
+print(favorite_food) #Ref for the output
+print("Is favorite_food == 'pizza'? I predict True.")
+print(favorite_food == 'pizza')
+print("\nIs favorite_food == 'pasta'? I predict False.")
+print(favorite_food == 'pasta')
+
+car_brand = 'bmw'
+print('\n' + car_brand) #Ref for the output
+print("Is car_brand == 'bmw'? I predict True.")
+print(car_brand == 'bmw')
+print("\nIs car_brand_brand == 'fiat'? I predict False.")
+print(car_brand == 'fiat')
+
+client_name = 'john'
+print('\n' + client_name) #Ref for the output
+print("Is client_name == 'john'? I predict True.")
+print(client_name == 'john')
+print("\nIs client_name == 'peter'? I predict False.")
+print(client_name == 'peter')
+
+soccer_team = 'fc barcelona'
+print('\n' + soccer_team) #Ref for the output
+print("Is soccer_team == 'fc barcelona'? I predict True.")
+print(soccer_team == 'fc barcelona')
+print("\nIs soccer_team == 'real madrid'? I predict False.")
+print(soccer_team == 'real madrid')
+
+language = 'english'
+print('\n' + language) #Ref for the output
+print("Is language == 'english'? I predict True.")
+print(language == 'english')
+print("\nIs language == 'spanish'? I predict False.")
+print(language == 'spanish')
