d622d72d0efc7868ee5cee20b13378484a9011bf
William <williamjaber94@gmail.com>
2021-10-20 13:09:11 -0500

    5-7. Favorite Fruit

---
+++ Chapter 5/favorite_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['strawberry', 'blackberry', 'orange']
+
+if 'strawberry' in favorite_fruits:
+    print('Strawberry is my favorite fruit for juice.')
+if 'blackberry' in favorite_fruits:
+    print('Blackberry has a great citrus flavor.')
+if 'orange' in favorite_fruits:
+    print('Orange is the best fruit')
+if 'mango' not in favorite_fruits:
+    print('I do like Mango but it is not in my favorites.')
+if 'pineapple' not in favorite_fruits:
+    print('I like pineapple, but not with pizza.')
