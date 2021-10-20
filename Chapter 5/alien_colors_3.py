769f986f948ce485a5a8057cdaaecb6010acb21c
William <williamjaber94@gmail.com>
2021-10-20 13:08:37 -0500

    5-5. Alien Colors #3

---
+++ Chapter 5/alien_colors_3.py
@@ -0,0 +1,24 @@
+# Write three versions of this program, making sure each message is printed for the appropriate color alien.
+alien_color = 'green'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien.')
+elif alien_color == 'yellow':
+    print('You earned 10 points for shooting the alien.')
+else:
+    print('You earned 15 points for shooting the alien.')
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien.')
+elif alien_color == 'yellow':
+    print('You earned 10 points for shooting the alien.')
+else:
+    print('You earned 15 points for shooting the alien.')
+
+alien_color = 'red'
+if alien_color == 'green':
+    print('You earned 5 points for shooting the alien.')
+elif alien_color == 'yellow':
+    print('You earned 10 points for shooting the alien.')
+else:
+    print('You earned 15 points for shooting the alien.')
