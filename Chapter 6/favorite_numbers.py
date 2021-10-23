cebf6408335ca233f8dc3671596f63556c877985
William <williamjaber94@gmail.com>
2021-10-22 20:47:16 -0500

    6-2. Favorite Numbers

---
+++ Chapter 6/favorite_numbers.py
@@ -0,0 +1,19 @@
+favorite_numbers = {
+    'kari': 1,
+    'dave': 2,
+    'dan': 3,
+    'alex': 4,
+    'edd': 5,
+    }
+
+
+for keys in favorite_numbers.keys():
+    print(keys)
+
+for values in favorite_numbers.values():
+    print(values)
+
+for k, v in favorite_numbers.items():
+    print(f'{k}: {v}')
+
+# print(favorite_numbers.items())
