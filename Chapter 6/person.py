d7143476f12c59c815ea56a5e6849fde7a63c6f0
William <williamjaber94@gmail.com>
2021-10-22 19:09:58 -0500

    6-1. Person

---
+++ Chapter 6/person.py
@@ -0,0 +1,11 @@
+dict_person = {
+    'f_name': 'Will',
+    'l_name': 'Jaber',
+    'age': 20,
+    'city': 'Medellin',
+    }
+
+print(dict_person.get('f_name'))
+print(dict_person.get('l_name'))
+print(dict_person.get('age'))
+print(dict_person.get('city'))
