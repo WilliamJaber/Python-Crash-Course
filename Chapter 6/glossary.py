27dc9801cd759aa367f66fa7d50ea988a5e01497
William <williamjaber94@gmail.com>
2021-10-24 18:21:49 -0500

    6-3. Glossary

---
+++ Chapter 6/glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    'Intger': 'is colloquially defined as a number that can be written without a fractional component.\n',
+    'Iterate': 'is the repetition of a process in order to generate a sequence of outcomes.\n',
+    'Indentation': 'is an empty space at the beginning of a line that groups particular blocks of code.\n',
+    'Concatinate': 'is the operation of joining character strings end-to-end.\n',
+    'Boolean': 'is a logical data type that can have only the values True or False.\n',
+    }
+
+for i in glossary:
+    print(f'{i}: {glossary[i]}')
