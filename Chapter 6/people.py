85ffee3d21a50d0ab687decdb4664953d2e45574
William <williamjaber94@gmail.com>
2021-10-25 12:57:48 -0500

    6-7. People

---
+++ Chapter 6/people.py
@@ -0,0 +1,25 @@
+person_1 = {
+    'f_name': 'Will',
+    'l_name': 'Jaber',
+    'age': 20,
+    'city': 'Medellin',
+    }
+
+person_2 = {
+    'f_name': 'Edd',
+    'l_name': 'Hamilton',
+    'age': 28,
+    'city': 'New York',
+    }
+
+person_3 = {
+    'f_name': 'Dave',
+    'l_name': 'Gomez',
+    'age': 30,
+    'city': 'Miami',
+    }
+
+people = [person_1, person_2, person_3]
+
+for person in people:
+    print(person)
