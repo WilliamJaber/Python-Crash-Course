7547ccd0a6bf5bbe9f8615465fd69152d76c1ddd
William <williamjaber94@gmail.com>
2021-10-25 13:08:52 -0500

    6-8. Pets

---
+++ Chapter 6/pets.py
@@ -0,0 +1,19 @@
+spike = {
+    'kind': 'bulldog',
+    'owner': 'tom',
+    }
+
+garfield = {
+    'kind': 'cat',
+    'owner': 'mark',
+    }
+
+tom = {
+    'kind': 'cat',
+    'owner': 'will',
+    }
+
+pets = [spike, garfield, tom]
+
+for pet in pets:
+    print(pet)
