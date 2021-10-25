01b196beacff70b2d0f9f1e1519dacbdd965681c
William <williamjaber94@gmail.com>
2021-10-24 19:45:44 -0500

    6-3. Glossary

--- Chapter 6/glossary.py
+++ Chapter 6/glossary.py
@@ -1,10 +1,13 @@
 glossary = {
-    'Intger': 'is colloquially defined as a number that can be written without a fractional component.\n',
-    'Iterate': 'is the repetition of a process in order to generate a sequence of outcomes.\n',
-    'Indentation': 'is an empty space at the beginning of a line that groups particular blocks of code.\n',
-    'Concatinate': 'is the operation of joining character strings end-to-end.\n',
-    'Boolean': 'is a logical data type that can have only the values True or False.\n',
+    'integer': 'is colloquially defined as a number that can be written without a fractional component.\n',
+    'iterate': 'is the repetition of a process in order to generate a sequence of outcomes.\n',
+    'indentation': 'is an empty space at the beginning of a line that groups particular blocks of code.\n',
+    'concatinate': 'is the operation of joining character strings end-to-end.\n',
+    'boolean': 'is a logical data type that can have only the values True or False.\n',
     }
 
-for word in glossary:
-    print(f'{word}: {glossary[word]}')
+print(f"Integer: {glossary.get('integer')}\n")
+print(f"Iterate: {glossary.get('iterate')}\n")
+print(f"Indentation: {glossary.get('indentation')}\n")
+print(f"Concatinate: {glossary.get('concatinate')}\n")
+print(f"Boolean: {glossary.get('boolean')}\n")
