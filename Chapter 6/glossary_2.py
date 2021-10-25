5fd412ea9256697f4f388fee3c12b818e7979fc6
William <williamjaber94@gmail.com>
2021-10-24 19:46:22 -0500

    6-4. Glossary 2

---
+++ Chapter 6/glossary_2.py
@@ -0,0 +1,14 @@
+glossary = {
+    'intger': 'is colloquially defined as a number that can be written without a fractional component.\n',
+    'iterate': 'is the repetition of a process in order to generate a sequence of outcomes.\n',
+    'indentation': 'is an empty space at the beginning of a line that groups particular blocks of code.\n',
+    'concatinate': 'is the operation of joining character strings end-to-end.\n',
+    'boolean': 'is a logical data type that can have only the values True or False.\n',
+    'loop': 'for loop iterates over an object until that object is complete.\n',
+    'tuple': 'is a immutable data structure that store an ordered sequence of values.\n',
+    'dictionary': 'is an unordered and mutable Python container that stores mappings of unique keys to values.\n',
+    'parse': 'is a command for dividing the given program code into a small piece of code for analyzing the correct syntax.',
+    }
+
+for k, v in glossary.items():
+    print(f'{k.title()}: {v}')
