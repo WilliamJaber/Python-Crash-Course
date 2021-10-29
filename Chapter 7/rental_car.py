694823a4641badc7fa93dbeff9c78632add3a48a
William <williamjaber94@gmail.com>
2021-10-29 14:33:08 -0500

    7-1. Rental Car

---
+++ Chapter 7/rental_car.py
@@ -0,0 +1,3 @@
+# Write a program that asks the user what kind of rental car they would like.
+car_input = input('Welcome, what kind of car would you like to rent?\n' )
+print(f"{car_input}?? sure, we have different models {car_input}'s let me show you!" )
