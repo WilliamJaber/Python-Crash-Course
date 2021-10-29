fb99ca75f56fd48fd6850caf4684bd5c5ac937f7
William <williamjaber94@gmail.com>
2021-10-29 15:26:07 -0500

    7-3. Multiples of Ten

--- Chapter 7/multiples_of_ten.py
+++ Chapter 7/multiples_of_ten.py
@@ -2,6 +2,6 @@ user_input = input('Insert a number:\n')
 user_input = int(user_input)
 
 if user_input % 10 == 0:
-    print('your number is multiple of ten.')
+    print('Your number is multiple of ten.')
 else:
-    print('sorry, your number is not multiple of ten.')
+    print('Sorry, your number is not multiple of ten.')
