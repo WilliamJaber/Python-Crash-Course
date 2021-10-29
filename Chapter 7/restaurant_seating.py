cb73865d7f1aa8b6eca9bdb5a5b954469392c1cc
William <williamjaber94@gmail.com>
2021-10-29 15:25:51 -0500

    7-2. Restaurant Seating

--- Chapter 7/restaurant_seating.py
+++ Chapter 7/restaurant_seating.py
@@ -1,4 +1,4 @@
-restaurant_seating = input('Good evening, how many people are in you dinner group?\n')
+restaurant_seating = input('Good evening, how many people are in your dinner group?\n')
 restaurant_seating = int(restaurant_seating)
 
 if restaurant_seating > 8:
