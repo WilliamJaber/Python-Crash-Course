c565bacce160ffc010d3c976ffdcadf0ef255bf0
William <williamjaber94@gmail.com>
2022-01-05 19:53:19 -0500

    8-1. Message

---
+++ Chapter 8/message.py
@@ -0,0 +1,7 @@
+def display_message():
+    """Prints a sentence of what I've learned"""
+
+    print(""">> Functions are blocks of code to perform a particular task and can be reused,
+makes your programs easier to write, read, test, and fix.""")
+
+display_message()
