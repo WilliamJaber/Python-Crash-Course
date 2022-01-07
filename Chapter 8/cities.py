4956a89d7e662c69e53af6ac14e652b8a4bcfa01
William <williamjaber94@gmail.com>
2022-01-07 18:10:56 -0500

    8-5. Cities

---
+++ Chapter 8/cities.py
@@ -0,0 +1,8 @@
+def describe_city(city, country = 'Colombia'):
+    print(f'{city.title()} is located in {country}.')
+
+# three different cities, at least one of which is not in the default country
+# at least one of which is not in the default country
+describe_city('Medellin')
+describe_city('Bogota')
+describe_city('Miami')
