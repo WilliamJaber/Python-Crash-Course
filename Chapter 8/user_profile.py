45fa61feb70d9c6a1df3ff6c8e73d2abb896a390
William <williamjaber94@gmail.com>
2022-01-12 12:03:59 -0500

    8-13. User Profile

---
+++ Chapter 8/user_profile.py
@@ -0,0 +1,10 @@
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return f'\n{user_info}\n'
+
+user_profile = build_profile('William', 'Jaber', age = 27,
+                location = 'South America', field = 'Programming')
+
+print(user_profile)
