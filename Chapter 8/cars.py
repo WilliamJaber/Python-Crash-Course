bb57f7a857e3192c6484448446767b49f060f814
William <williamjaber94@gmail.com>
2022-01-12 12:07:54 -0500

    8-14. Cars, updating annotations

--- Chapter 8/cars.py
+++ Chapter 8/cars.py
@@ -1,5 +1,6 @@
-def make_car(manufacturer, model, **extras):
+def make_car(manufacturer, model, **extras) -> dict:
     """Builds a dictionary containing information about a car"""
+    
     extras['manufacturer'] = manufacturer
     extras['model'] = model
     return f'\n{extras}\n'
