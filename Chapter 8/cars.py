48243b43a44bba270402d5c962eac6c947c52dcf
William <williamjaber94@gmail.com>
2022-01-12 12:04:08 -0500

    8-14. Cars

---
+++ Chapter 8/cars.py
@@ -0,0 +1,10 @@
+def make_car(manufacturer, model, **extras):
+    """Builds a dictionary containing information about a car"""
+    extras['manufacturer'] = manufacturer
+    extras['model'] = model
+    return f'\n{extras}\n'
+
+car_info = make_car('BMW', 'X5', color = 'white',
+                seats_color = 'Red', luxury_wheels = True)
+
+print(car_info)
