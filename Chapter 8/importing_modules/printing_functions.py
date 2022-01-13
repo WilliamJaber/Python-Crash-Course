aa1eb18d7af24fba4a3459c60d12bb56a24d7691
William <williamjaber94@gmail.com>
2022-01-13 18:45:18 -0500

    main module function

---
+++ Chapter 8/importing_modules/printing_functions.py
@@ -0,0 +1,4 @@
+def describe_city(city, country = 'Colombia') -> str:
+    """Prints the name of a city located in a default country in this case Colombia"""
+
+    print(f'{city.title()} is located in {country}.')
