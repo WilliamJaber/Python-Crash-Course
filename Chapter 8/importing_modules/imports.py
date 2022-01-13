b0cb006e477b63621ca22f8255ed79864de5fd3a
William <williamjaber94@gmail.com>
2022-01-13 18:45:50 -0500

    8-16. Imports

---
+++ Chapter 8/importing_modules/imports.py
@@ -0,0 +1,13 @@
+import printing_functions
+from printing_functions import describe_city
+from printing_functions import describe_city as dc
+import printing_functions as pf
+from printing_functions import *
+
+printing_functions.describe_city('cartagena')
+
+describe_city('cartagena')
+
+dc('cartagena')
+
+pf.describe_city('cartagena')
