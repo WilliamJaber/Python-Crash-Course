d9b00e61baeae64b4fb152d1eab05eb5e7cebeef
William <williamjaber94@gmail.com>
2022-01-19 14:01:42 -0500

    9-1. Restaurant: Updated to Restaurant_type.py

---
+++ Chapter 9/restaurant_type.py
@@ -0,0 +1,22 @@
+class Restaurant:
+    """Represent different restaurants name, type and an open message"""
+
+    def __init__(self, rest_name = str, cuisine_type = str) -> str:
+        self.name = rest_name
+        self.type = cuisine_type
+
+    def describe_restaurant(self) -> str:
+        """Prints the description of the restaurant: name and cuisine type"""
+
+        print(f"{self.name.title()} is a restaurant that serves {self.type} food.\n")
+
+    def open_restaurant(self) -> str:
+        """Prints a message indicating that the restaurant is open"""
+
+        print(f"{self.name.title()} is open at lunch and dinner time from Tuesday to Sunday.\n")
+
+restaurant = Restaurant("bonhomia", "fusion")
+print(f"\nName: {restaurant.name.title()}, Cuisine type: {restaurant.type.title()}.\n")
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
