544371f97b8fab70f1f8342846d561c6bf2b51ad
William <williamjaber94@gmail.com>
2022-01-22 20:36:29 -0500

    pending

---
+++ Chapter 9/python_module_of_the_week.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+9-16. Python Module of the Week: One excellent resource for exploring the
+Python standard library is a site called Python Module of the Week. Go to
+https://pymotw.com/ and look at the table of contents.
+
+Find a module that
+looks interesting to you and read about it, perhaps starting with the random
+module.
+
+
+Styling Classes
+A few styling issues related to classes are worth clarifying, especially as your
+programs become more complicated.
+
+
+Class names should be written in CamelCase. To do this, capitalize the
+first letter of each word in the name, and don’t use underscores. Instance
+and module names should be written in lowercase with underscores between
+words.
