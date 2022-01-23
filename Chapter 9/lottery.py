2b6ea3ae5d0baa3103d4269b96e6b707ee4b72af
William <williamjaber94@gmail.com>
2022-01-22 20:35:06 -0500

    9-14. Loterry Update

--- Chapter 9/lottery.py
+++ Chapter 9/lottery.py
@@ -1,15 +1,10 @@
-from random import choice
+from random import choices
 
-def main():
-    """Randomly choose between numbers and letters for lottery"""
+def lottery():
+    """Randomly choose four values between numbers and letters for lottery"""
 
-    valid_numbers = [4, 8, 2, 6, 7, 5, 1, 3, 0, 9, "A", "B", "C", "D", "E"]
-    winning_ticket = []
+    valid_numbers = ["4", "8", "2", "6", "7", "5", "1", "3", "0", "9", "A", "B", "C", "D", "E"]
+    winning_ticket = choices(valid_numbers, k=4)
+    print(f"\nAny ticket matching this sequence of four numbers and letters wins a prize.\n{winning_ticket}\n")
 
-    n = 0
-    while n < 4:
-        n += 1
-        result = winning_ticket.append(choice(valid_numbers))
-    print(f"\nAny ticket matching these sequence of numbers and letters wins a prize.\n{winning_ticket}\n")
-
-main()
+lottery()
